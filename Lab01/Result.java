4bc5b48b99f096f05057e096130e89556681c331
iqraghani0450 <49375539+iqraghani0450@users.noreply.github.com>
2019-04-08 01:18:00 +0500

    Add files via upload

---
+++ Lab01/Result.java
@@ -0,0 +1,32 @@
+class Result
+{
+public static void main(String args[])
+{
+	String Name;
+	double percentage;
+	int marks;
+	char grade;
+	boolean status;
+Name="Iqra";
+percentage=86.15;
+marks=560;
+grade='A';
+status=true;
+
+System.out.println("Your name is "+Name);
+System.out.println("Your percentage in last semester was "+percentage+"%");
+System.out.println("Your marks in last semester "+marks);
+System.out.println("Your grade in last semester was "+grade);
+if(status==true)
+{
+System.out.println("Your status in last semester is pass");
+}
+else
+{
+System.out.println("Your status in last semester is fail");
+}
+}
+}
+
+
+
---
+++ Lab01/Separate.java
@@ -0,0 +1,10 @@
+class Separate
+{
+public static void main(String args[])
+{
+float a=50.05f;
+System.out.println(a);
+System.out.println((int)a);
+System.out.println(a-(int)a);
+}
+}
