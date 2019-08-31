fb83b66ddc606419690f86aafbb7d8cb191f3a12
iqraghani0450 <49375539+iqraghani0450@users.noreply.github.com>
2019-08-31 17:58:27 +0500

    Add files via upload

---
+++ Task2 lab08/Main.java
@@ -0,0 +1,11 @@
+    
+class Main
+{
+	public static void main(String args[])
+	{
+		Soccer s =new Soccer();
+		Cricket c=new Cricket();
+		s.getNumberOfTeamMembers();
+		c.getNumberOfTeamMembers();
+	}
+}
